2b3e3107d749448da6ecb4c5444c49c9d06cb245
GiseelaDaiana <Gisela.daiana@live.com>
2023-11-09 00:26:14 -0300

    subHeader

--- src/components/Comision.jsx
+++ src/components/Comision.jsx
@@ -3,11 +3,12 @@ import { Alert } from "@material-ui/lab";
 import { useEffect } from "react";
 import { useState } from "react";
 import { getDataFromBackend } from "../constants/curso";
-import { getCurso as getCurso_fake } from '../services/curso-fake';
+import { getCurso as  getTodosLosUsuarios_fake } from '../services/curso-fake';
 import {conteinerButton} from "../style/buttonStyle"
-import {getCursoPorIdProfesor, getTodosLasCursos} from '../services/curso'
+import {getCursoPorIdProfesor} from '../services/curso'
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import { NavLink } from "react-router-dom/cjs/react-router-dom";
+import { SubHeader } from "./SubHeader";
 
 
 const useStyles = makeStyles(() => ({
@@ -22,20 +23,25 @@ const useStyles = makeStyles(() => ({
 export default function Comision() {
   const {estadoCurso}  = useParams();
   const classes = useStyles();
-  console.log('estado:', {estadoCurso})
+
 
   const [comision, setComision] = useState(null);
   const [hasError, setHasError] = useState(false);
+  const tituloHeader = ( estadoCurso === 'actual' ?
+     'Listado De Cursos |cuatrimestre actual ' :
+     'Listado De Cursos | cuatrimestre anterior')
+
+  
 
 
   useEffect(() => {
-    console.log('useEfect')
     async function fetchCommision() {
-        console.log('useEfect1')
-
+        const getFunction = getDataFromBackend
+        ? getCursoPorIdProfesor("6539f56c21db6cc57698b95e")
+        : getTodosLosUsuarios_fake;
       try {
         // Agregar el ID del profesor segun la informacion que tengas en tu base de datos local.
-        const commision = await getCursoPorIdProfesor("6539f56c21db6cc57698b95e");
+        const commision = await getFunction();
         setComision(commision);
       } catch (err) {
         console.log("Ocurrio este error.", err);
@@ -43,7 +49,7 @@ export default function Comision() {
       }
     }
     fetchCommision();
-  }, []);
+  }, []);0
 
   const comisionRendering = () => {
     console.log('comisionrend')
@@ -52,14 +58,13 @@ export default function Comision() {
         <Card className={classes.card}>
           <CardContent>
           <Container className={classes.curso} maxWidth="xl">
-
-                  <Container>
-                    {estadoCurso === 'actual' ? <Typography variant="h4" >Listado De Cursos |cuatrimestre actual </Typography>:<Typography variant="h4" >Listado De Cursos | cuatrimestre anterior </Typography> }
-                  </Container>
+                  <SubHeader titulo= {tituloHeader}/>
+           
                   <Container maxWidth="xl" className={classes.conteinerButton}>
                     {comision.map((it) => (
+                      //Falta Nombtr  de la  Materia 
                     <Button   variant="contained"  key={it._id}>
-                        Falta Name Materia id:{` ${it.comision}`}
+                        id Materia: {` ${it.idMateria}`} |{` ${it.Comision}`}
                         </Button>
 
                     ))}
--- src/components/SubHeader.jsx
+++ src/components/SubHeader.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Typography } from "@material-ui/core";
 
-export function SubH({ titulo }) {
+export function SubHeader({ titulo }) {
   return (
     <>
       <Typography variant="h4">{titulo}</Typography>
@@ -10,6 +10,6 @@ export function SubH({ titulo }) {
   );
 }
 
-SubH.propTypes = {
+SubHeader.propTypes = {
   titulo: PropTypes.string.isRequired, // Asegura que 'titulo' sea una cadena (string) y es requerido.
 };
