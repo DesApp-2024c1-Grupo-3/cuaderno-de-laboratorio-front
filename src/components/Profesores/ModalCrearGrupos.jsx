c84d6ea5f5d912036cb54025717ddd8ad1173661
GiseelaDaiana <Gisela.daiana@live.com>
2023-11-28 21:23:16 -0300

    modal

--- src/components/Profesores/ModalCrearGrupos.jsx
+++ src/components/Profesores/ModalCrearGrupos.jsx
@@ -33,8 +33,8 @@ const useStyles = makeStyles(() => ({
 }));
 
 const style = {
-  position: 'absolute',
-  top: '50%',
+  position: 'fixed',
+  top: '35%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
   width: 400,
