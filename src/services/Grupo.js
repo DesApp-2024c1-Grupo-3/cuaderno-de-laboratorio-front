ec9b23e2e5df43188c2af6b02e6b9ba80d0d1f6d
GiseelaDaiana <Gisela.daiana@live.com>
2023-12-06 02:00:28 -0300

    newPost

--- src/services/Grupo.js
+++ src/services/Grupo.js
@@ -1,4 +1,4 @@
-import { getJsonFromApi, postJsonToApi } from './utils';
+import { deleteFromApi, getJsonFromApi, postJsonToApi } from './utils';
 
 export async function getTodosLosGrupos() {
     const apiResponse = await getJsonFromApi('grupos/');
@@ -8,14 +8,27 @@ export async function getTodosLosGrupos() {
     return apiResponse.arrayGrupos;
 }
 
+export async function getGrupoByCursoId(id) {
+    const apiResponse = await getJsonFromApi(`curso/${id}/grupos`);
+    return apiResponse.grupos;
+}
+
 export async function getGrupoPorId(id) {
     const apiResponse = await getJsonFromApi(`grupo/${id}`);
     return apiResponse.grupo;
 }
 
-export async function postCrearGrupo(data) {
+export async function postCrearGrupo(data, idCurso) {
 
-    const apiResponse = await postJsonToApi(`grupo`, data);
+    const apiResponse = await postJsonToApi(`grupos/${idCurso}`, data);
+    console.log('api', apiResponse.data)
+    return apiResponse;
+
+}
+export async function postEliminarGrupo(id) {
+
+    console.log(id, 'deleteApi')
+    const apiResponse = await deleteFromApi(`grupo${id}`);
     return apiResponse;
 
 }
