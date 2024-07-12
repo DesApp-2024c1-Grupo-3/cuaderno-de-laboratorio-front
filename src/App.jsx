51f230720fa9067ba2bd2b88235751a71cb0207f
ayortedra <cesaraugusto.pacheco@estudiantes.unahur.edu.ar>
2024-07-11 22:07:54 -0300

    Ultima linea

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import CrearTpsBeta from './components/CrearTpsBeta';
 import AdministrarGrupos from './components/Profesores/AdmGrupoBeta';
 import TpDetalleBeta from './components/TpDetalleBeta';
 
-import CalificarGrupo from './components/CalificarGrupo';
+
 import CalificarGrupo from './components/CalificarGrupo';
 import CalificarAlumno from './components/CalificarAlumno';
 //---------------------------Componentes Alumno---------------------------------------
