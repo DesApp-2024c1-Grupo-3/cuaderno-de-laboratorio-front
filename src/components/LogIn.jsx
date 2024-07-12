072630e1c779db8402b7e8e1b6a063eaee37a573
aeotero <popal9193@gmail.com>
2024-07-11 23:04:01 -0300

    comenzada la logica para traer con dni profesor o alumno id

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 import { Button, Card, CardContent, Container, TextField, Typography, Box } from '@mui/material';
 import { NavLink } from 'react-router-dom';
+import { getTodosLosProfesores, getProfesorPorId } from '../services/Profesor';
+import { getTodosLosAlumnos, getAlumnoPorId, getAlumnoById, GetIdAlumnoByDNI } from '../services/Alumnos';
+import { getTodosLasCursos } from '../services/curso';
 
 const LogIn = () => {
   const [dni, setDni] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
 
   const handleDniChange = (e) => {
     setDni(e.target.value);
@@ -12,6 +16,40 @@ const LogIn = () => {
 
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
+  };
+
+  // logica para buscar dni entre profesor y alumno
+  const handleLogin = async () => {
+    //try {
+      setError(null); // Reset error
+
+      const profesores = await getTodosLosProfesores();
+      console.log("lo que trae profesores: " + profesores);
+
+      // const cursos = await getTodosLasCursos();
+      // console.log("lo que trae todo los cursos" + cursos);
+
+      const alumnos = await getTodosLosAlumnos();
+      console.log("lo que trae por dni: " + alumnos);
+
+      //const profesor = profesores.find(prof => prof.dni === parseInt(dni));
+      //if (profesor) {
+      //  navigate(`/comision/actual`);
+      //  return;
+     // }
+
+      //const alumnos = await getTodosLosAlumnos();
+    //   const alumno = alumnos.find(alum => alum.dni === parseInt(dni));
+    //   if (alumno) {
+    //     navigate(`/alumno/curso`);
+    //     return;
+    //   }
+
+    //   setError('Usuario no encontrado');
+    // } catch (error) {
+    //   console.error('Error al buscar el usuario', error);
+    //   setError('Ocurrió un error al intentar ingresar. Por favor, intenta nuevamente.');
+    // }
   };
 
   return (
@@ -40,21 +78,20 @@ const LogIn = () => {
           />
           <Box mt={2}>
             <Button
-              component={NavLink}
-              to={`/comision/actual`}
+              onClick={handleLogin}
               variant="contained"
               fullWidth
               sx={{ mb: 2 }}
             >
-              Ingresar (Prof C act)
+              Ingresar
             </Button>
             <Button
               component={NavLink}
-              to={`/alumno/curso`}
+              to={'/#'}
               variant="text"
               fullWidth
             >
-              Olvidé la contraseña (Al C act)
+              Olvidé la contraseña
             </Button>
           </Box>
         </Container>
